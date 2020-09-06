2ca61f4210b23a30f2c8d16ed614501583294b9d
ravin.poudel <admin@admins-Air.hsd1.ct.comcast.net>
2020-09-06 13:56:18 -0400

    added man

--- run.R
+++ run.R
@@ -67,7 +67,7 @@ toc()
 #############
 #Introduction to pkgdown
 # Run once to configure package to use pkgdown
-usethis::use_pkgdown()
+#usethis::use_pkgdown()
 # Run to build the website
 pkgdown::build_site()
 
