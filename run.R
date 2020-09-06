cbb26d16d07ffb06239d3b66d5addb81d4beea0c
ravin.poudel <admin@admins-Air.hsd1.ct.comcast.net>
2020-09-06 12:25:20 -0400

    deleted

--- run.R
+++ run.R
@@ -64,10 +64,12 @@ tic()
 rnorm(1000,0,1)
 toc()
 
-
-
-
-
+#############
+#Introduction to pkgdown
+# Run once to configure package to use pkgdown
+usethis::use_pkgdown()
+# Run to build the website
+pkgdown::build_site()
 
 
 
